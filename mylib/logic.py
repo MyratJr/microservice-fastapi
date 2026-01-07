99785f2607f584514daa4f9e0dc74f66a06dc237
Begmyradov Myratgeldi <begmyradowmyratgeldi@gmail.com>
2026-01-07 12:49:52 +0500

    adding a new cli

--- mylib/logic.py
+++ mylib/logic.py
@@ -5,3 +5,9 @@ def wiki(name="Cristiano Ronaldo", length=1):
     """This is a wikipedia fetcher"""
     my_wiki = wikipedia.summary(name, length)
     return my_wiki
+
+
+def search_wiki(name):
+    """This is a wikipedia search"""
+    my_wiki = wikipedia.search(name)
+    return my_wiki
